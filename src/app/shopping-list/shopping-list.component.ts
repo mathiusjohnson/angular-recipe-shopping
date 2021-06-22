6f48b28ddd11d022052fb0249879e4a37fcd0964
Mathius <devachievem@gmail.com>
2021-06-22 15:06:12 -0700

    implement ondestroy and onedit methods in shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -8,16 +9,27 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slServce: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slServce.getIngredients();
-    this.slServce.ingredientsChanged
+    this.subscription = this.slServce.ingredientsChanged
       .subscribe((ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       })
+  }
+
+  onEditItem(index: number) {
+    console.log('index in onedit: ', index);
+
+    this.slServce.startedEditing.next(index);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
