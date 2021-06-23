566e21b6a2ccf64d887e754c2955255dbdafac8c
Mathius <devachievem@gmail.com>
2021-06-23 11:46:12 -0700

    add authservice to data storage service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
 import { root } from "rxjs/internal-compatibility";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
-import { map, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: root})
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
   storeRecipe() {
     const recipes = this.recipeService.getRecipes();
@@ -20,17 +21,18 @@ export class DataStorageService {
   fetchRecipes() {
     return this.http
       .get<Recipe[]>('https://ng-course-recipe-book-11905-default-rtdb.firebaseio.com/recipes.json')
-      .pipe(map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
-          }
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            }
+          })
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes)
         })
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes)
-      })
-    )
+      )
   }
 }
