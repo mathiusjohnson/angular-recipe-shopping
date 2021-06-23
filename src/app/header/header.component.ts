1332bf05d8047d48cced1cf16c2bc05f285e882f
Mathius <devachievem@gmail.com>
2021-06-23 10:04:25 -0700

    add login handler to header component ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from '@angular/core';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-
 export class HeaderComponent {
-  collapsed = true;
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipe();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 }
