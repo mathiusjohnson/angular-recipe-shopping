51db0e9e48609bc20a5f52dcbe2656077bf56c7e
Mathius <devachievem@gmail.com>
2021-06-24 13:58:23 -0700

    Create SL actions and reducer

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,21 +1,30 @@
-import { Ingredient } from "../../shared/ingredient.model";
-import * as ShoppingListActions from './shopping-list.actions'
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import * as ShoppingListActions from './shopping-list.actions';
+
 const initialState = {
-  ingredients: [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ]
-}
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)]
+};
 
 export function shoppingListReducer(
-  state = initialState, action:
-  ShoppingListActions.AddIngredient
-) {
+  state = initialState,
+  action: ShoppingListActions.ShoppingListActions
+  ) {
+    console.log('state in reducer: ', state);
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
+
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload]
-      }
+      };
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      };
+    default:
+      console.log(state);
+
+      return state;
   }
 }
