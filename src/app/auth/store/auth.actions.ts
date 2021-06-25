c116f96377b156607883b2788fab2342d71350ed
Mathius <devachievem@gmail.com>
2021-06-25 12:51:12 -0700

    add more actions to auth actions

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -3,8 +3,11 @@ import { Action } from "@ngrx/store";
 export const LOGIN_START = '[Auth] Login Start';
 export const AUTHENTICATE_SUCCESS = '[Auth] Login';
 export const AUTHENTICATE_FAIL = '[Auth] Login Failed'
+export const SIGNUP_START = '[Auth] Signup Start';
+export const CLEAR_ERROR = '[Auth] Clear Error';
+export const AUTO_LOGIN = '[Auth] Auto Login';
 export const LOGOUT = '[Auth] Logout';
-export const SIGNUP_START = '[Auth] Signup Start'
+
 
 export class AuthenticateSuccess implements Action {
   readonly type = AUTHENTICATE_SUCCESS;
@@ -41,4 +44,19 @@ export class SignupStart implements Action {
   constructor(public payload: { email: string; password: string; }) {}
 }
 
-export type AuthActions = AuthenticateSuccess | Logout | LoginStart | AuthenticateFail | SignupStart;
+export class ClearError implements Action {
+  readonly type = CLEAR_ERROR;
+}
+
+export class AutoLogin implements Action {
+  readonly type = AUTO_LOGIN;
+}
+
+export type AuthActions =
+  | AuthenticateSuccess
+  | Logout
+  | LoginStart
+  | AuthenticateFail
+  | SignupStart
+  | ClearError
+  | AutoLogin;
