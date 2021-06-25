473aad1c45afd7c3dcee80c50d209ba316545504
Mathius <devachievem@gmail.com>
2021-06-25 15:34:14 -0700

    add recipe actions to header for fetch and save

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
 import { Store } from "@ngrx/store";
 import { map } from 'rxjs/operators';
 
-import { DataStorageService } from '../shared/data-storage.service';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from "../auth/store/auth.actions";
+import * as RecipeActions from "../recipes/store/recipe.action";
 
 @Component({
   selector: 'app-header',
@@ -17,8 +16,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription;
 
   constructor(
-    private dataStorageService: DataStorageService,
-    private authService: AuthService,
     private store: Store<fromApp.AppState>
     ) {}
 
@@ -32,11 +29,11 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes();
+    this.store.dispatch(new RecipeActions.StoreRecipes());
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipeActions.FetchRecipes())
   }
 
   onLogout() {
