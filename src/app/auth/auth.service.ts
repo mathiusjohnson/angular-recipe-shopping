1c986bbc6ec7ed5326c780f1048b17c390799636
Mathius <devachievem@gmail.com>
2021-06-25 12:10:32 -0700

    refactor auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -87,7 +87,7 @@ export class AuthService {
     )
 
     if (loadedUser.token) {
-      this.store.dispatch(new AuthActions.Login({
+      this.store.dispatch(new AuthActions.AuthenticateSuccess({
           email: loadedUser.email,
           userId: loadedUser.id,
           token: loadedUser.token,
@@ -127,7 +127,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000)
     const user = new User(email, userId, token, expirationDate)
     // this.user.next(user)
-    this.store.dispatch(new AuthActions.Login({
+    this.store.dispatch(new AuthActions.AuthenticateSuccess({
       email,
       userId,
       token,
